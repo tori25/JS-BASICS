ffe6da38a51e9d63f27038706b43cbca5fc7c37c
Vera Mezhvynskiy <vera.mezhvynskiy@gmail.com>
2023-04-05 13:39:35 +0300

    Filtering, Mapping, Reducing, Sorting

--- arrays.js
+++ arrays.js
@@ -54,4 +54,62 @@ first.forEach((number, index) => console.log(index, number)); //for-each
 //Joining arrays
 const joined = first.join(","); //Adding a string to [] and returning a string
 
-console.log(joined);
+const message = "This is my first message!";
+const parts = message.split(" "); //Separate string using split method return [].
+const combined2 = parts.join("-");
+
+//Sorting arrays of numbers
+const numbers3 = [3, 5, 7, 8, 2, 4, 1];
+numbers3.sort();
+numbers3.reverse();
+
+//Sorting arrays of obj
+const courses2 = [
+  {
+    id: 1,
+    name: "Node.js",
+  },
+  {
+    id: 2,
+    name: "javaScript",
+  },
+];
+
+courses2.sort(function (a, b) {
+  const nameA = a.name.toUpperCase();
+  const nameB = b.name.toUpperCase();
+
+  if (nameA < nameB) return -1;
+  if (nameA > nameB) return 1;
+  return 0;
+});
+
+//Testing the elem of an []
+const allPositive = numbers3.every((value) => value > 0);
+const atLeastOnePositive = numbers3.some((value) => value > 0);
+
+//Filtering an []
+let filtered = numbers3.filter((value) => value >= 0);
+
+//Mapping the elem of an [] - numbers
+let maped = filtered.map((n) => "<li>" + n + "</li>");
+const html = "<ul>" + maped.join("") + "</ul>";
+
+//Mapping the elem of an [] - obj
+let items = filtered.map((n) => ({ value: n }));
+
+//Short way to do filter and map together - chaining
+const items2 = numbers3
+  .filter((value) => value >= 0)
+  .map((n) => ({ value: n }))
+  .filter((obj) => obj.value > 1)
+  .map((obj) => obj.value);
+
+//Reducing the elem of an []
+let sum = 0;
+for (let n of numbers3) sum += n; //calc the sum of all the numb in []
+
+//second method to calc sum
+const sum2 = numbers3.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
